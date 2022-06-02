d14931e47ef195aeec8de0f039ef4a1df92a3348
karimberdiDekhkonov <karimberdidehqonov0921@gmail.com>
2022-06-03 00:34:47 +0200

    commit 5

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -1,13 +1,6 @@
 package uz.pdp;
 
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendLocation;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.*;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import uz.pdp.bots.myServiceBot;
--- src/main/java/uz/pdp/bots/myServiceBot.java
+++ src/main/java/uz/pdp/bots/myServiceBot.java
@@ -16,13 +16,14 @@ public class myServiceBot extends TelegramLongPollingBot {
     public String getBotUsername() {
         return "WebServiceUzBot";
     }
-    static boolean auto = true;
+
     @Override
     public String getBotToken() {
         return "5543864112:AAFEB_qj2IqLC8CSXI2eB5Cz_2ZcCBADQEM";
     }
 
     public static int users = 0;
+    static boolean auto = true;
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -32,41 +33,50 @@ public class myServiceBot extends TelegramLongPollingBot {
         String text = message.getText();
 
 
+        switch (text) {
+            case "/start":
+                users++;
+                sendMessage.setText("Sizga qanday yordam bera olamiz ?\nYani qanday platforma yaratmoqchisiz?");
 
-        if (text.equals("/start")) {
-            users++;
-            sendMessage.setText("Sizga qanday yordam bera olamiz ?\nYani qanday platforma yaratmoqchisiz?");
-
-            ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup();
-            sendMessage.setReplyMarkup(markup);
-            markup.setResizeKeyboard(true);
-            markup.setOneTimeKeyboard(true);
-            markup.setSelective(true);
-            List<KeyboardRow> keyboardRows = new ArrayList<>();
-            KeyboardRow row1 = new KeyboardRow();
-            KeyboardRow row2 = new KeyboardRow();
-            row1.add("Telegram Bot");
-            row1.add("Website");
-            row2.add("Korxonani Tizimlashtirish");
-            keyboardRows.add(row1);
-            keyboardRows.add(row2);
-            markup.setKeyboard(keyboardRows);
-        } else if (text.equals("getUsers0921")) {
-            sendMessage.setText("Umumiy startlar soni: " + users);
-        } else if (text.equals("Telegram Bot")) {
-            sendMessage.setText("Tushunarli \nBot bajarishi kerak bo'lgan vazifalarni qisqacha yoritib o'ting!");
-        }else if (text.equals("Website")) {
-            sendMessage.setText("Tushunarli \nWebsite bajarishi kerak bo'lgan vazifalarni qisqacha yoritib o'ting!");
-        }else {
-            if (auto) {
-                sendMessage.setText("Hurmatli " + message.getChat().getFirstName() + " murojatingiz qabul qilindi yaqin daqiqqalar ichida siz bilan bog'lanamiz \uD83D\uDE0A\uD83D\uDE0A ");
-                auto=false;
-            }else {
-                sendMessage.setText("");
-            }
+                ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup();
+                sendMessage.setReplyMarkup(markup);
+                markup.setResizeKeyboard(true);
+                markup.setOneTimeKeyboard(true);
+                markup.setSelective(true);
+                List<KeyboardRow> keyboardRows = new ArrayList<>();
+                KeyboardRow row1 = new KeyboardRow();
+                KeyboardRow row2 = new KeyboardRow();
+                row1.add("Telegram Bot");
+                row1.add("Website");
+                row2.add("Korxonani Tizimlashtirish");
+                keyboardRows.add(row1);
+                keyboardRows.add(row2);
+                markup.setKeyboard(keyboardRows);
+                break;
+            case "getUsers0921":
+                sendMessage.setText("Umumiy startlar soni: " + users);
+                break;
+            case "Telegram Bot":
+                sendMessage.setText("Tushunarli \nBot bajarishi kerak bo'lgan vazifalarni qisqacha yoritib o'ting!");
+                auto = true;
+                break;
+            case "Website":
+                sendMessage.setText("Tushunarli \nWebsite bajarishi kerak bo'lgan vazifalarni qisqacha yoritib o'ting!");
+                auto = true;
+                break;
+            case "Korxonani Tizimlashtirish":
+                sendMessage.setText("Tushunarli \nTizim bajarishi kerak bo'lgan vazifalarni qisqacha yoritib o'ting!");
+                auto = true;
+                break;
+            default:
+                if (auto) {
+                    sendMessage.setText("Hurmatli " + message.getChat().getFirstName() + " murojatingiz qabul qilindi yaqin daqiqqalar ichida siz bilan bog'lanamiz \uD83D\uDE0A\uD83D\uDE0A ");
+                    auto = false;
+                } else {
+                    sendMessage.setText(" ");
+                    return;
+                }
         }
-
-
 
         try {
             execute(sendMessage);
